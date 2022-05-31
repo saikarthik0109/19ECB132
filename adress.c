bead44130ba788edd8c33acb0916317c453d0f8a
saikarthik0109 <101512604+saikarthik0109@users.noreply.github.com>
2022-05-31 19:01:08 +0530

    with output

--- adress.c
+++ adress.c
@@ -16,3 +16,15 @@ int main()
    printf("Value of c: %d\n\n", c); 
    return 0;
 }
+/* output;
+Address of c: 0x7ffd108a5e2c
+Value of c: 22
+
+Address of pointer pc: 0x7ffd108a5e2c
+Content of pointer pc: 22
+
+Address of pointer pc: 0x7ffd108a5e2c
+Content of pointer pc: 11
+
+Address of c: 0x7ffd108a5e2c
+Value of c: 2 */
