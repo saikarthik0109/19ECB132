43f1fa327cedbffc3ad55a8140a4a3fde23d6a0b
saikarthik0109 <101512604+saikarthik0109@users.noreply.github.com>
2022-05-31 19:15:04 +0530

    with output

--- singlelimkedlist.c
+++ singlelimkedlist.c
@@ -103,4 +103,8 @@ int main() {
   printf("\nSorted List: ");
   printList(head);
 }
-
+/* output;
+Linked list:  3  2  5  1  4 
+After deleting an element:  2  5  1  4 
+3 is not found
+Sorted List:  1  2  4  5 /*
